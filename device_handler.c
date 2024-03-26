30fa736ab38435f6d69ae7c3dacc896e8bd486e2
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-03-26 01:37:00 +0000

    Continue with SNMP code 10212

--- device_handler.c
+++ device_handler.c
@@ -265,7 +265,7 @@ device_handler_loop(void *arg)
           dev->next_ping_time = time_now + DEVICE_KEEPALIVE_DURATION_SEC;
           dev->status = snmp_get_printer_status(g_dev_handler.button_conn, buf,
                                                 sizeof(buf), dev->ip);
-          if (dev->status != 10001 && dev->status != 10006
+          if (dev->status != 10001 && dev->status != 10006 && dev->status != 10212 
 		  && dev->status != 40000 && dev->status != 40038) {
             dev->next_ping_time = time_now + DEVICE_OFFLINE_RETRY_DURATION_SEC;
             dev->next_register_time = 0;
@@ -274,7 +274,7 @@ device_handler_loop(void *arg)
           }
         }
 
-        if (dev->status != 10001 && dev->status != 10006
+        if (dev->status != 10001 && dev->status != 10006 && dev->status != 10212
 	    && dev->status != 40000 && dev->status != 40038) {
           continue;
         }
